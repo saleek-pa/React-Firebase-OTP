507c69a6a8f0c8c50506844995bab1e19e567c10
Mohamed Saleek PA <saleek.pa@gmail.com>
2024-01-03 17:40:38 +0530

    refactor: clean up logs

--- Client/src/pages/Login.js
+++ Client/src/pages/Login.js
@@ -6,120 +6,114 @@ import OtpInput from "react-otp-input";
 import Axios from "axios";
 
 const axios = Axios.create({
-   baseURL: "http://localhost:4000",
-   headers: {
-      "Content-Type": "application/json",
-   },
+  baseURL: "http://localhost:4000",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 const Login = () => {
-   const [login, setLogin] = useState(true);
-   const [phone, setPhone] = useState("+917025356979");
-   const [otp, setOtp] = useState("");
-   const [userId, setUserId] = useState("")
-   const navigate = useNavigate();
+  const [login, setLogin] = useState(true);
+  const [phone, setPhone] = useState("+91");
+  const [otp, setOtp] = useState("");
+  const [userId, setUserId] = useState("");
+  const navigate = useNavigate();
 
-   const generateRecaptcha = () => {
-      window.recaptchaVerifier = new RecaptchaVerifier(auth, "recaptcha", {
-         size: "invisible",
-         callback: (response) => console.log(response),
-      });
-   };
+  const generateRecaptcha = () => {
+    window.recaptchaVerifier = new RecaptchaVerifier(auth, "recaptcha", {
+      size: "invisible",
+      callback: (response) => console.log(response),
+    });
+  };
 
-   const handleSubmit = async (e) => {
-      try {
-         e.preventDefault();
-         generateRecaptcha();
-         const appVerifier = window.recaptchaVerifier;
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      generateRecaptcha();
+      const appVerifier = window.recaptchaVerifier;
 
-         const response = await axios.post("/api/users/check", { phone });
-         console.log(response)
-         if (response.data.success) {
-            console.log("first")
-            setUserId(response.data.userId)
-            console.log(userId)
-            const confirmationResult = await signInWithPhoneNumber(auth, phone, appVerifier);
-            window.confirmationResult = confirmationResult;
-            setLogin(false);
-         } else {
-            alert("Incorrect number or user not registered");
-         }
-      } catch (error) {
-         console.error(error);
+      const response = await axios.post("/api/users/check", { phone });
+      if (response.data.success) {
+        setUserId(response.data.userId);
+        const confirmationResult = await signInWithPhoneNumber(auth, phone, appVerifier);
+        window.confirmationResult = confirmationResult;
+        setLogin(false);
+      } else {
+        alert("Incorrect number or user not registered");
       }
-   };
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-   const handleVerifyOtp = async (e) => {
-      try {
-         e.preventDefault();
-         const confirmationResult = window.confirmationResult;
-         const result = await confirmationResult.confirm(otp);
-         console.log(result)
-         const user = result.user;
-         console.log(user);
+  const handleVerifyOtp = async (e) => {
+    try {
+      e.preventDefault();
+      const confirmationResult = window.confirmationResult;
+      await confirmationResult.confirm(otp);
 
-         const response = await axios.post("/api/users/login", { userId });
-         if (response.data.success) {
-            const token = response.data.token;
-            console.log(token);
-            alert("Success");
-            navigate("/home");
-         } else {
-            alert("Failed");
-         }
-      } catch (error) {
-         console.error(error);
+      const response = await axios.post("/api/users/login", { userId });
+      if (response.data.success) {
+        const token = response.data.token;
+        console.log(token);
+        alert("Success");
+        navigate("/home");
+      } else {
+        alert("Failed");
       }
-   };
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-   return (
-      <>
-         {login ? (
-            <div className="container">
-               <div className="form-container">
-                  <div className="logo-container">Login</div>
+  return (
+    <>
+      {login ? (
+        <div className="container">
+          <div className="form-container">
+            <div className="logo-container">Login</div>
 
-                  <form className="form" onSubmit={handleSubmit}>
-                     <div className="form-group">
-                        <label>Phone</label>
-                        <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} required />
-                     </div>
+            <form className="form" onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label>Phone</label>
+                <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} required />
+              </div>
+              <button className="form-submit-btn" type="submit">
+                Continue
+              </button>
+            </form>
 
-                     <button className="form-submit-btn" type="submit">
-                        Continue
-                     </button>
-                  </form>
-
-                  <p className="signup-link">
-                     <a href=" " className="signup-link link">
-                        Login with password
-                     </a>
-                  </p>
-               </div>
-               <div id="recaptcha"></div>
+            <p className="signup-link">
+              <a href=" " className="signup-link link">
+                Login with password
+              </a>
+            </p>
+          </div>
+          <div id="recaptcha"></div>
+        </div>
+      ) : (
+        <div className="container">
+          <form className="form">
+            <div className="title">OTP</div>
+            <div className="title">Verification Code</div>
+            <p className="message">We have sent a verification code to your mobile number</p>
+            <div className="inputs">
+              <OtpInput
+                value={otp}
+                onChange={setOtp}
+                numInputs={6}
+                renderInput={(props) => <input {...props} />}
+                shouldAutoFocus
+              />
             </div>
-         ) : (
-            <div className="container">
-               <form className="form">
-                  <div className="title">OTP</div> <div className="title">Verification Code</div>
-                  <p className="message">We have sent a verification code to your mobile number</p>
-                  <div className="inputs">
-                     <OtpInput
-                        value={otp}
-                        onChange={setOtp}
-                        numInputs={6}
-                        renderInput={(props) => <input {...props} />}
-                        shouldAutoFocus
-                     />
-                  </div>
-                  <button className="form-submit-btn" type="submit" onClick={handleVerifyOtp}>
-                     Verify
-                  </button>
-               </form>
-            </div>
-         )}
-      </>
-   );
+            <button className="form-submit-btn" type="submit" onClick={handleVerifyOtp}>
+              Verify
+            </button>
+          </form>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Login;
--- Server/index.js
+++ Server/index.js
@@ -2,60 +2,59 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-// --------- Connect to MongoDB ---------
+// ========= Connect to MongoDB =========
 mongoose.connect(process.env.MONGODB_URI);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection error: "));
 db.once("open", () => console.log("Connected Successfully"));
 
-// ---- Define User Schema and Model ----
+// ==== Define User Schema and Model ====
 const userSchema = new mongoose.Schema({
-   name: String,
-   phone: String,
+  name: String,
+  phone: String,
 });
 
 const User = mongoose.model("User", userSchema);
 
-// --- Route to check whether user is exist or not in DB ---
+// === Route to check whether user is exist or not in DB ===
 app.post("/api/users/check", async (req, res) => {
-   const { phone } = req.body;
-   try {
-      const user = await User.findOne({ phone });
-      if (user) {
-         console.log("check success")
-         res.status(200).json({ success: true, userId: user._id });
-      } else {
-         res.status(404).json({ success: false });
-      }
-   } catch (error) {
-      res.status(500).json({ error: "Internal Server Error" });
-   }
+  const { phone } = req.body;
+  try {
+    const user = await User.findOne({ phone });
+    if (user) {
+      res.status(200).json({ success: true, userId: user._id });
+    } else {
+      res.status(404).json({ success: false });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
+// ========= Route to generate JWT token =========
 app.post("/api/users/login", async (req, res) => {
-   const { userId } = req.body;
-   try {
-      const token = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
-      if (token) {
-         console.log("jwt success")
-         res.status(200).json({ success: true, token });
-      } else {
-         res.status(404).json({ success: false });
-      }
-   } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Internal Server Error" });
-   }
+  const { userId } = req.body;
+  try {
+    const token = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
+    if (token) {
+      res.status(200).json({ success: true, token });
+    } else {
+      res.status(404).json({ success: false });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-   console.log(`Server is running on ${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
